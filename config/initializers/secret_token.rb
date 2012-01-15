82e77730484c4dc1c0bb4559ecf9e5a89dd503d5
James Hicks <jhicks0300@gmail.com>
2012-01-14 22:54:24 -0600

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EchoToTheResque::Application.config.secret_token = '28e56fb1820477e320adf51df0d2a5d1f2dcdf891b4d3092d41a067b586c1fbd8ae49db644ca0a03bdf526b92fd58edbff406a5247c329f4a97f3ab0fc3eb0db'
